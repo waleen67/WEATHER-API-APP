0c0ef58a4b9a93771de6772dd33e779ea602cba8
waleen67 <265734393+waleen67@users.noreply.github.com>
2026-03-09 17:06:59 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,64 @@
+import sys
+import requests
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QMessageBox, QVBoxLayout
+from PyQt5.QtCore import Qt
+
+class WeatherApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.city_label = QLabel("enter city name", self)
+        self.city_input = QLineEdit(self)
+        self.get_weather_button = QPushButton("get weather", self)
+        self.temperature_label = QLabel("enter temperature", self)
+        self.emoji_label = QLabel("⭐", self)
+        self.description_label = QLabel("sunny", self)
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("Weather App")
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.city_label)
+        vbox.addWidget(self.city_input)
+        vbox.addWidget(self.get_weather_button)
+        vbox.addWidget(self.temperature_label)
+        vbox.addWidget(self.emoji_label)
+        vbox.addWidget(self.description_label)
+
+        self.setLayout(vbox)
+
+        self.city_label.setAlignment(Qt.AlignCenter)
+        self.city_input.setAlignment(Qt.AlignCenter)
+        self.temperature_label.setAlignment(Qt.AlignCenter)
+        self.emoji_label.setAlignment(Qt.AlignCenter)
+        self.description_label.setAlignment(Qt.AlignCenter)
+
+        self.city_label.setObjectName("city_label")
+        self.city_input.setObjectName("city_input")
+        self.get_weather_button.setObjectName("get_weather_button")
+        self.temperature_label.setObjectName("temperature_label")
+        self.emoji_label.setObjectName("emoji_label")
+        self.description_label.setObjectName("description_label")
+
+        self.setStyleSheet("""
+        QLabel, QPushButton {
+            font-family: Calibri;
+        }
+
+        QLabel#city_label {
+            font-size: 40px;
+            font-style: italic;
+        }
+
+        QLineEdit#city_input {
+            font-size: 40px;
+            font-weight: bold;
+        }
+        """)
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    Weather_app = WeatherApp()
+    Weather_app.show()
+    sys.exit(app.exec_())
